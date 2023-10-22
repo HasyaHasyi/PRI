3e92a5c5ade9d169bb8d335a98080e9126c5ccad
Muhammad Haikal <98247943+haikaaal21@users.noreply.github.com>
2023-10-22 17:20:38 +0800

    Navbar + PRI

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+
+const buyButton = document.getElementById("buyButton");
+
+buyButton.addEventListener("click", () => {
+    // code to execute when the button is clicked
+    console.log("Buy button clicked!");
+});
